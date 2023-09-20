7a97e9439f17bec093d5cc8d95807d3ab42addd4
ionelasty6 <ionelasty6@gmail.com>
2023-09-20 08:43:49 +0300

    додавання функціоналу

--- Program.cs
+++ Program.cs
@@ -63,6 +63,50 @@ class Address
     }
 }
 
+class Converter
+{
+    private double usdRate;
+    private double eurRate;
+    private double plnRate;
+
+    public Converter(double usd, double eur, double pln)
+    {
+        usdRate = usd;
+        eurRate = eur;
+        plnRate = pln;
+    }
+
+    public double ConvertToUSD(double uah)
+    {
+        return uah / usdRate;
+    }
+
+    public double ConvertToEUR(double uah)
+    {
+        return uah / eurRate;
+    }
+
+    public double ConvertToPLN(double uah)
+    {
+        return uah / plnRate;
+    }
+
+    public double ConvertFromUSD(double usd)
+    {
+        return usd * usdRate;
+    }
+
+    public double ConvertFromEUR(double eur)
+    {
+        return eur * eurRate;
+    }
+
+    public double ConvertFromPLN(double pln)
+    {
+        return pln * plnRate;
+    }
+}
+
 class Program
 {
     static void Main()
@@ -80,6 +124,21 @@ class Program
 
         // Виведення інформації про адресу
         address.PrintAddress();
+
+        // Створення екземпляру класу Converter з курсами валют
+        Converter converter = new Converter(36.5, 39.5, 8.5);
+
+        // Конвертація з гривні в USD
+        double uahAmount = 1000.0;
+        double usdAmount = converter.ConvertToUSD(uahAmount);
+        Console.WriteLine($"{uahAmount} UAH = {usdAmount} USD");
+
+        // Конвертація з USD в гривню
+        double usdToConvert = 50.0;
+        double uahConverted = converter.ConvertFromUSD(usdToConvert);
+        Console.WriteLine($"{usdToConvert} USD = {uahConverted} UAH");
+
+        // Аналогічно можна виконати конвертацію в EUR та PLN
 
         Console.ReadLine();
     }
